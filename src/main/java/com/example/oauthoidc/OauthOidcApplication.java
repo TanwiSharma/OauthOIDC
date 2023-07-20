19ec4591ecaed8717686b49bb93fbcd4b6209dba
TanwiSharma <tanwisharma1989@gmail.com>
2023-07-21 00:22:23 +0530

    initial commit

---
+++ src/main/java/com/example/oauthoidc/OauthOidcApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthoidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthOidcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthOidcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauthoidc/OauthOidcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthoidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthOidcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
