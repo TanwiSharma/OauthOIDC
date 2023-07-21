81eee11d0765dde95bf7b8858caa25497dd99d8e
TanwiSharma <tanwisharma1989@gmail.com>
2023-07-22 02:27:09 +0530

    initial commit

--- src/main/java/com/example/LoginComponent/LoginController/PlainLogin.java
+++ src/main/java/com/example/LoginComponent/LoginController/PlainLogin.java
@@ -1,2 +1,15 @@
-package com.example.LoginComponent.LoginController;public class PlainLogin {
+package com.example.LoginComponent.LoginController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/*pain login controller*/
+@Controller("/plainLogin")
+public class PlainLogin {
+    @RequestMapping("/login.do")
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        return modelAndView;
+    }
 }
--- src/main/java/com/example/LoginComponent/LoginModel/User.java
+++ src/main/java/com/example/LoginComponent/LoginModel/User.java
@@ -1,2 +1,64 @@
-package com.example.LoginComponent.LoginModel;public class User {
+package com.example.LoginComponent.LoginModel;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    private String fName;
+    private String lName;
+    private String location;
+
+    public User() {
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
 }
